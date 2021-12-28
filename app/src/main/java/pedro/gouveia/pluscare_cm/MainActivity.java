1e87b21d2e4713cb9294aed8ea281ed65f05bd9d
jpcruz <jpcruz@student.dei.uc.pt>
2021-12-28 21:45:46 +0000

    O tablayout está a funcionar. Se correr o programa agora, vai dar para testar.

--- app/src/main/java/pedro/gouveia/pluscare_cm/MainActivity.java
+++ app/src/main/java/pedro/gouveia/pluscare_cm/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void switchActivities() {
-        Intent switchActivityIntent = new Intent(this, LoginActivity.class);
+        Intent switchActivityIntent = new Intent(this, UtilizadorInfo.class);
         startActivity(switchActivityIntent);
     }
 
--- app/src/main/java/pedro/gouveia/pluscare_cm/UtilizadorInfo.java
+++ app/src/main/java/pedro/gouveia/pluscare_cm/UtilizadorInfo.java
@@ -1,15 +1,60 @@
 package pedro.gouveia.pluscare_cm;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.viewpager2.widget.ViewPager2;
 
 import com.google.android.material.tabs.TabItem;
 import com.google.android.material.tabs.TabLayout;
 
-public class UtilizadorInfo extends Fragment {
+public class UtilizadorInfo extends AppCompatActivity {
 
     private TabLayout tabLayout;
-    private TabItem dadosInfo, dadosSaude;
-    private ViewPager2 viewPager2;
+    private FragmentAdapter fragmentAdapter;
+    private ViewPager2 viewPage;
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.utente_info_layout);
+
+        tabLayout = findViewById(R.id.tabLayout);
+        viewPage = findViewById(R.id.viewPager2);
+
+        FragmentManager fm = getSupportFragmentManager();
+        fragmentAdapter = new FragmentAdapter(fm, getLifecycle());
+        viewPage.setAdapter(fragmentAdapter);
+
+        tabLayout.addTab(tabLayout.newTab().setText(getString(R.string.dadosUtente)));
+        tabLayout.addTab(tabLayout.newTab().setText(getString(R.string.dadosSaudeUtente)));
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPage.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        viewPage.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                tabLayout.selectTab(tabLayout.getTabAt(position));
+            }
+        });
+
+    }
 }
