bd6d28bba97d4fb8610e8116431811a11576f874
jpcruz <jpcruz@student.dei.uc.pt>
2021-12-30 23:21:16 +0000

    Adicionei a página inicial admin

--- app/src/main/java/pedro/gouveia/pluscare_cm/AdminMainActivity.java
+++ app/src/main/java/pedro/gouveia/pluscare_cm/AdminMainActivity.java
@@ -1,15 +1,50 @@
 package pedro.gouveia.pluscare_cm;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AdminMainActivity extends AppCompatActivity {
 
+    private TextView numTarefa, numMedicamento, numOcorrencia, numHigiene;
+    private Button buttonTarefas, buttonMedicamentos, buttonOcorrencias, buttonHigiene;
+
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.admin_activity_layout);
+
+        numTarefa = findViewById(R.id.numTarefas);
+        numMedicamento = findViewById(R.id.numMedicamentos);
+        numOcorrencia = findViewById(R.id.numOcorrencias);
+        numHigiene = findViewById(R.id.numHigiene);
+
+        buttonTarefas = findViewById(R.id.buttonListTarefas);
+        buttonMedicamentos = findViewById(R.id.buttonListMedicamentos);
+        buttonHigiene = findViewById(R.id.buttonListTarefas);
+        buttonOcorrencias = findViewById(R.id.buttonListOcorrencias);
+
+        buttonTarefas.setOnClickListener(view -> {
+            //Mostrar lista de tarefas
+
+        });
+
+        buttonMedicamentos.setOnClickListener(view -> {
+            //Mostrar lista de Medicamentos
+
+        });
+
+        buttonTarefas.setOnClickListener(view -> {
+            //Mostrar lista de Tarefas
+
+        });
+
+        buttonOcorrencias.setOnClickListener(view -> {
+            //Mostrar lista de Ocorrencias
+
+        });
 
     }
 
